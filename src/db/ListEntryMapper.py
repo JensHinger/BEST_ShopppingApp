4ac01979ec0cfd7f0034800b1fe6485bc43ce374
Jens Hinger <jh221@hdm-stuttgart.de>
2020-05-18 14:47:59 +0200

    Funktionen noch nicht alle getestet

--- src/db/ListEntryMapper.py
+++ src/db/ListEntryMapper.py
@@ -13,47 +13,107 @@ class ListEntryMapper(Mapper):
 
         if len(tuples) == 1:
 
-            for (id, name, creation_date, item, retailer, user) in tuples:
+            for (id, name, creation_date, item_id, retailer_id, user_id, list_id) in tuples:
                 listentry = ListEntry()
                 listentry.set_id(id)
                 listentry.set_name(name)
                 listentry.set_creation_date(creation_date)
-                listentry.set_item(item)
-                listentry.set_retailer(retailer)
-                listentry.set_user(user)
+                listentry.set_item_id(item_id)
+                listentry.set_retailer_id(retailer_id)
+                listentry.set_user_id(user_id)
+                listentry.set_list_id(list_id)
                 result = listentry
 
         else:
 
-            for (id, name, creation_date, item, retailer, user) in tuples:
+            for (id, name, creation_date, item_id, retailer_id, user_id, list_id) in tuples:
                 listentry = ListEntry()
                 listentry.set_id(id)
                 listentry.set_name(name)
                 listentry.set_creation_date(creation_date)
-                listentry.set_item(item)
-                listentry.set_retailer(retailer)
-                listentry.set_user(user)
+                listentry.set_item_id(item_id)
+                listentry.set_retailer_id(retailer_id)
+                listentry.set_user_id(user_id)
+                listentry.set_list_id(list_id)
                 result = listentry
 
         return result
 
     def find_all(self):
-        pass
+
+        result = []
+
+        cursor = self._cnx.cursor()
+        command = "SELECT * FROM listentry"
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        result = self.build_bo(tuples)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
 
     def find_by_id(self, id):
-        pass
 
-    def insert(self, object):
-        pass
+        result = None
 
-    def update(self, object):
-        pass
+        cursor = self._cnx.cursor()
+        command = "SELECT id, name, creation_date, item_id, retailer_id, user_id, list_id FROM listentry" \
+                  " WHERE id LIKE ('{}')".format(id)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
 
-    def delete(self, object):
-        pass
+        result = self.build_bo(tuples)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def insert(self, listentry):
+
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) as maxid from listentry")
+        tuples = cursor.fetchall()
+
+        for (maxid) in tuples:
+            listentry.set_id(maxid[0] + 1)
+
+        command = "INSERT INTO party (id, name, creation_date, item_id, retailer_id, user_id, list_id) VALUES " \
+                  "('{}','{}','{}','{}','{}','{}','{}')"\
+                .format(listentry.get_id(), listentry.get_name(), listentry.get_creation_date(), listentry.get_item_id()
+                        , listentry.get_retailer_id(), listentry.get_user_id(), listentry.get_list_id())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
+
+    def update(self, listentry):
+
+        cursor = self._cnx.cursor()
+        command = "UPDATE listentry SET name = ('{}'), creation_date = ('{}'), item_id = ('{}'), retailer_id = ('{}'),"\
+                  " user_id = ('{}'), list_id = ('{}') WHERE id = ('{}')" \
+            .format(listentry.get_name(), listentry.get_creation_date(), listentry.get_item_id()
+                    , listentry.get_retailer_id(), listentry.get_user_id(), listentry.get_list_id(), listentry.get_id())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
+
+    def delete(self, listentry):
+
+        cursor = self._cnx.cursor()
+        command = "DELETE FROM listentry WHERE id LIKE ('{}')".format(listentry.get_id())
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        self._cnx.commit()
+        cursor.close()
 
 
 if __name__ == "__main__":
     with ListEntryMapper() as mapper:
-        result = mapper.find_all()
+        result = mapper.find_by_id(4)
         print(result)
