3239129889f4d314b6da3c6b6fd80b4c8faeedb9
Jens Hinger <jh221@hdm-stuttgart.de>
2020-05-18 13:09:04 +0200

    List bo Namen der Funktionen korrigiert

--- src/bo/List.py
+++ src/bo/List.py
@@ -7,10 +7,10 @@ class List(BusinessObject):
         super().__init__()
         self._party = None
 
-    def get_group(self):
+    def get_party(self):
         """Auslesen der Gruppe in welcher die Liste existiert"""
         return self._party
 
-    def set_group(self, party):
+    def set_party(self, party):
         """Setzen der Gruppe in welcher die Liste erstellt wurde"""
         self._party = party
