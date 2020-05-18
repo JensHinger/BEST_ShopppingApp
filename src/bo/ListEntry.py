d0a9b1ed702abab9f1b492dc13576bd164e989b9
Jens Hinger <jh221@hdm-stuttgart.de>
2020-05-18 14:31:12 +0200

    Variablennamen korrigiert!

--- src/bo/ListEntry.py
+++ src/bo/ListEntry.py
@@ -5,30 +5,30 @@ class ListEntry(BusinessObject):
 
     def __init__(self):
         super().__init__()
-        self._item = None
-        self._retailer = None
-        self._user = None
+        self._item_id = None
+        self._retailer_id = None
+        self._user_id = None
 
-    def get_item(self):
+    def get_item_id(self):
         """Auslesen des Produkts in dem Listeneintrag"""
-        return self._item
+        return self._item_id
 
-    def set_item(self, item):
+    def set_item_id(self, item_id):
         """Setzen des Produktes in dem Listeneintrag"""
-        self._item = item
+        self._item_id = item_id
 
-    def get_retailer(self):
+    def get_retailer_id(self):
         """Auslesen des Einkaufsladen in dem Listeneintrag"""
-        return self._retailer
+        return self._retailer_id
 
-    def set_retailer(self, retailer):
+    def set_retailer_id(self, retailer_id):
         """Setzen des Einkaufsladen in dem Listeneintrag"""
-        self._retailer = retailer
+        self._retailer_id = retailer_id
 
-    def get_user(self):
+    def get_user_id(self):
         """Auslesen des Zugehörigen Käufer in dem Listeneintrag"""
-        return self._user
-    
-    def set_user(self, user):
+        return self._user_id
+
+    def set_user_id(self, user_id):
         """Setzen des Zugehörigen Käufer in dem Listeneintrag"""
-        self._user = user
+        self._user_id = user_id
