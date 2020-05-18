1e4243bcce84639b6d9138fa03aca4fbd20175e2
Jens Hinger <jh221@hdm-stuttgart.de>
2020-05-18 13:19:54 +0200

    Variablen name von party -> partyl_id geändert

--- src/bo/List.py
+++ src/bo/List.py
@@ -7,10 +7,10 @@ class List(BusinessObject):
         super().__init__()
         self._partyl_id = None
 
-    def get_party(self):
+    def get_partyl_id(self):
         """Auslesen der Gruppe in welcher die Liste existiert"""
         return self._partyl_id
 
-    def set_party(self, party):
+    def set_partyl_id(self, party):
         """Setzen der Gruppe in welcher die Liste erstellt wurde"""
         self._partyl_id = party
