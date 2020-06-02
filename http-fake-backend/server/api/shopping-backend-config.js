721bc73bc80f9161748c73227f1fab49a94a0a86
Jens Hinger <jh221@hdm-stuttgart.de>
2020-06-02 19:21:25 +0200

    Alle .json hinzugefügt

--- http-fake-backend/server/api/shopping-backend-config.js
+++ http-fake-backend/server/api/shopping-backend-config.js
@@ -4,11 +4,54 @@ const prefix = "shopping"
 
 module.exports = SetupEndpoint({
     name: 'shopping',
-    urls: [{
+    urls: [
+        {
+        params: '/invitation',
+        requests: [{
+            method: 'GET',
+            response: '/response-files/shopping/invitation.json'
+        }]
+    }, {
         params: '/item',
         requests: [{
             method: 'GET',
             response: '/response-files/shopping/item.json'
+        }]
+    }, {
+        params: '/list',
+        requests: [{
+            method: 'GET',
+            response: '/response-files/shopping/list.json'
+        }]
+    }, {
+        params: '/listentry',
+        requests: [{
+            method: 'GET',
+            response: '/response-files/shopping/listentry.json'
+        }]
+    }, {
+        params: '/party',
+        requests: [{
+            method: 'GET',
+            response: '/response-files/shopping/party.json'
+        }]
+    }, {
+        params: '/retailer',
+        requests: [{
+            method: 'GET',
+            response: '/response-files/shopping/retailer.json'
+        }]
+    }, {
+        params: '/standardlistentry',
+        requests: [{
+            method: 'GET',
+            response: '/response-files/shopping/standardlistentry.json'
+        }]
+    }, {
+        params: '/user',
+        requests: [{
+            method: 'GET',
+            response: '/response-files/shopping/user.json'
         }]
     }]
 });
