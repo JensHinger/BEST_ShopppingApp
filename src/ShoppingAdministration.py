773816cb6a8f7af3b6b5b902ddc82a2c750dc27c
ReneHofmann <65234386+ReneHofmann@users.noreply.github.com>
2020-06-10 16:03:44 +0200

    ShoppingAdmin fertiggestellt

--- src/ShoppingAdministration.py
+++ src/ShoppingAdministration.py
@@ -1,10 +1,18 @@
 from src.bo.List import List
 from src.bo.ListEntry import ListEntry
 from src.bo.Item import Item
+from src.bo.Invitation import Invitation
+from src.bo.Party import Party
+from src.bo.User import User
+from src.bo.StandardListEntry import StandardListEntry
 
 from src.db.ListMapper import ListMapper
 from src.db.ListEntryMapper import ListEntryMapper
 from src.db.ItemMapper import ItemMapper
+from src.db.InvitationMapper import InvitationMapper
+from src.db.PartyMapper import PartyMapper
+from src.db.UserMapper import UserMapper
+from src.db.StandardListEntryMapper import StandardListEntryMapper
 
 """
 Autoren: 
@@ -145,3 +153,188 @@ class ShoppingAdministration(object):
         with ItemMapper() as mapper:
             mapper.delete(item)
 
+    """ 
+    Hier geht es um die Invitation
+    """
+
+    def get_all_invitation(self):
+        """Alle Invitations auslesen."""
+        with InvitationMapper() as mapper:
+            return mapper.find_all()
+
+    def get_invitation_by_id(self, id):
+        """Invitations nach id auslesen."""
+        with InvitationMapper() as mapper:
+            return mapper.find_by_id(id)
+
+    def get_all_user_in_party(self, partyi_id):
+        """Alle User einer Party auslesen."""
+        with InvitationMapper() as mapper:
+            return mapper.find_all_user_in_party(partyi_id)
+
+    def get_pen_invites_by_target_user(self, target_user_id):
+        """Alle pending-Invitations von dem Target-User auslesen."""
+        with InvitationMapper() as mapper:
+            return mapper.find_pend_invites_by_target_user(target_user_id)
+
+    def get_pen_invites_by_source_user(self, source_user_id):
+        """Alle Invitations von Source-User auslesen."""
+        with InvitationMapper() as mapper:
+            return mapper.find_pend_invites_by_source_user(source_user_id)
+
+    def get_source_user_by_id(self, source_user_id):
+        """Den source-user auslesen."""
+        with InvitationMapper() as mapper:
+            return mapper.find_source_user(source_user_id)
+
+    def get_target_user_by_id(self, target_user_id):
+        """Den target-user auslesen."""
+        with InvitationMapper() as mapper:
+            return mapper.find_target_user(target_user_id)
+
+    def get_all_parties_corr_user(self, target_user_id):
+        """Alle Parties zu denen ein User gehört auslesen."""
+        with InvitationMapper() as mapper:
+            return mapper.find_all_parties_corr_user(target_user_id)
+
+    def get_all_pend_invites(self):
+        """Alle pending-invitations auslesen."""
+        with InvitationMapper() as mapper:
+            return mapper.find_all_pend_invites()
+
+    def create_invitation(self, partyi_id, target_user_id, source_user_id):
+        """Eine Invitation erstellen."""
+        invitation = Invitation()
+        invitation.set_partyi_id(partyi_id)
+        invitation.set_target_user_id(target_user_id)
+        invitation.set_source_user_id(source_user_id)
+
+        with InvitationMapper() as mapper:
+            mapper.insert(invitation)
+
+    def update_invitation(self, invitation):
+        """Invitation updaten/speichern."""
+        with InvitationMapper() as mapper:
+            mapper.update(invitation)
+
+    def delete_invitation(self, invitation):
+        """Eine Invitation löschen."""
+        with InvitationMapper() as mapper:
+            mapper.delete(invitation)
+
+    """
+    Hier geht es um die Party
+    """
+
+    def get_all_parties(self):
+        """Alle Gruppen auslesen."""
+        with PartyMapper() as mapper:
+            mapper.find_all()
+
+    def get_party_by_id(self, id):
+        """Eine Gruppe anhand der ID auslesen."""
+        with PartyMapper() as mapper:
+            mapper.find_by_id(id)
+
+    def create_party(self, name):
+        """Eine Gruppe erstellen."""
+        party = Party()
+        party.set_name(name)
+
+        with PartyMapper() as mapper:
+            mapper.insert(party)
+
+    def update_party(self, party):
+        """Eine Gruppe updaten."""
+        with PartyMapper() as mapper:
+            mapper.update(party)
+
+    def delete_party(self, party):
+        """Eine Gruppe löschen."""
+        with PartyMapper() as mapper:
+            mapper.delete(party)
+
+    """
+    Hier geht es um den User
+    """
+
+    def get_all_users(self):
+        """Alle User auslesen."""
+        with UserMapper() as mapper:
+            mapper.find_all()
+
+    def get_user_by_email(self, email):
+        """Einen User nach der email auslesen."""
+        with UserMapper() as mapper:
+            mapper.find_user_by_email(email)
+
+    def get_user_by_google_id(self, google_id):
+        """Einen User nach der Google-ID auslesen."""
+        with UserMapper() as mapper:
+            mapper.find_user_by_google_id(google_id)
+
+    def get_user_by_id(self, id):
+        """Einen User nach seiner ID auslesen"""
+        with UserMapper() as mapper:
+            mapper.find_by_id(id)
+
+    def create_user(self, name, email, google_id):
+        """Einen User erstellen."""
+        user = User()
+        user.set_name(name)
+        user.set_email(email)
+        user.set_google_id(google_id)
+
+        with UserMapper() as mapper:
+            mapper.insert(user)
+
+    def update_user(self, user):
+        """Einen User updaten."""
+        with UserMapper() as mapper:
+            mapper.update(user)
+
+    def delete_user(self, user):
+        """Einen User löschen."""
+        with UserMapper() as mapper:
+            mapper.delete(user)
+
+    """
+    Hier geht es um den Standartlisteneinträge
+    """
+
+    def get_all_standard_list_entrys(self):
+        """Alle Standartlisteneinträge auslesen."""
+        with StandardListEntryMapper() as mapper:
+            mapper.find_all()
+
+    def get_standard_list_entry_by_id(self, id):
+        """Standartlisteneintrag nach ID auslesen."""
+        with StandardListEntryMapper() as mapper:
+            mapper.find_by_id(id)
+
+    def get_standard_list_entry_by_party_id(self, party_id):
+        """Standartlisteneinträge nach Gruppen-ID auslesen."""
+        with StandardListEntryMapper() as mapper:
+            mapper.find_by_party_id(party_id)
+
+    def create_standard_list_entry(self, name, item_id, retailer_id, user_id, party_id):
+        """Ein Standartlisteneintrag erstellen."""
+        standardlistentry = StandardListEntry()
+        standardlistentry.set_name(name)
+        standardlistentry.set_item_id(item_id)
+        standardlistentry.set_retailer_id(retailer_id)
+        standardlistentry.set_user_id(user_id)
+        standardlistentry.set_party_id(party_id)
+
+        with StandardListEntryMapper() as mapper:
+            mapper.insert(standardlistentry)
+
+    def update_standard_list_entry(self, standardlistentry):
+        """Einen Standartlisteneintrag updaten."""
+        with StandardListEntryMapper() as mapper:
+            mapper.update(standardlistentry)
+
+    def delete_standard_list_entry(self, standardlistentry):
+        """Einen Standartlisteneintrag löschen."""
+        with StandardListEntryMapper() as mapper:
+            mapper.delete(standardlistentry)
