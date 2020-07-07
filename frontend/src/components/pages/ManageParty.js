3eda73aef38dd415e437449281ffcbe303a09c78
Jens Hinger <jh221@hdm-stuttgart.de>
2020-07-07 17:15:13 +0200

    Delete Invitation added

--- frontend/src/components/pages/ManageParty.js
+++ frontend/src/components/pages/ManageParty.js
@@ -3,6 +3,9 @@ import { Typography, Button, Grid, Divider, TextField} from '@material-ui/core';
 import ShoppingAPI from '../../api/ShoppingAPI'
 import CreateGroupDialog from '../dialogs/CreateGroupDialog';
 import ExitGroupDialog from '../dialogs/ExitGroupDialog';
+import RemoveGroupMemberDialog from '../dialogs/RemoveGroupMemberDialog'
+
+//Hier muss noch das update rein sobald User gelöscht wird muss neu gerendert werden.
 
 class ManageGroup extends Component{
     constructor(props){
@@ -10,45 +13,68 @@ class ManageGroup extends Component{
 
         this.state = {
             party : null,
-            users : null
+            users : [],
+            invitations : []
         }
     }
 
     componentDidMount(){
         this.getParty()
-        this.getAllUsers()
     }
 
     getParty = () => {
         ShoppingAPI.getAPI().getPartyById(2)
-        .then(party => this.setState({party : party}))
+        .then(function(party) { 
+            this.setState({party : party});
+            this.getAllUsersInParty(party.getID())
+        }.bind(this))
     }
 
-    getAllUsers = () => {
-        ShoppingAPI.getAPI().getAcceptedInvitationsByPartyId(2)
-        .then(invitations => invitations.map = (invitation) => (
-            ShoppingAPI.getAPI().getUserById(invitation.getID())
-            .then(user => this.setState(state => state.users.push(user)))
-        ))
+    getAllUsersInParty = (id) => {
+        ShoppingAPI.getAPI().getAcceptedInvitationsByPartyId(id)
+        .then(function(invitations) {
+            this.setState({invitations : invitations})   
+            invitations.map((invitation) => (
+            ShoppingAPI.getAPI().getUserById(invitation.getTargetUserId())
+            .then(user => this.setState({
+                users: [...this.state.users, user]
+           })
+           )) 
+        )}.bind(this))
+    }
 
+    handleUserDelete = (index) => {
+        let array = this.state.users
+
+        array.splice(index, 1)
+        this.setState({users : array})
     }
 
     render() {
 
         const currentParty = this.state.party
-
+        const users = this.state.users
         return(
         <Typography variant='h6' component='h1' align='center'>
             <br margin-top = '20px'/>
-            Gruppe verwalten
+                Gruppe verwalten
             <Divider/>
             <CreateGroupDialog/>
             <br margin-top = '20px'/>
-            Gruppennamen ändern
+                Gruppennamen ändern
             <Divider/>
             <TextField id ="outlined-basic" label = "Name ändern" variant = "outlined"/>
             <br margin-top = '20px'/>
-            Gruppenmitglieder
+                Gruppenmitglieder
+                {users? 
+                    users.map((user, index) =>
+                    <Grid>
+                        {user.getName()}
+                        {user.getID()}
+                        <RemoveGroupMemberDialog invitation = {this.state.invitations.filter(invitation => invitation.getTargetUserId() === user.getID())} handleInvitationDelete = {this.handleUserDelete} index = {index}/>
+                    </Grid>
+                    )
+                :null}
             <Divider/>
             <br margin-top = '20px'/>
             <ExitGroupDialog/>
