c4bc37f79ff8bb2dbfd35212882de37bf51b8f1b
Jens Hinger <jh221@hdm-stuttgart.de>
2020-07-21 09:11:23 +0200

    Funktionalität hinzugefügt

--- report/src/components/pages/Report.js
+++ report/src/components/pages/Report.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {ThemeProvider} from "@material-ui/core";
 import Theme from "../../Theme";
-import {Button, TextField, Typography, Divider, Grid, ListItemIcon} from '@material-ui/core';
+import {Typography, Divider, Grid, TableContainer, TableHead, TableRow, TableCell, TableBody, Table, Paper} from '@material-ui/core';
 import ShoppingAPI from '../../api/ShoppingAPI'
 
 
@@ -40,8 +40,8 @@ class Report extends Component {
 
 render (){
 
-    console.log("Item :", this.state.mostBoughtItem)
-    console.log("Retailer :", this.state.mostVisitedRetailer)
+    const items = this.state.mostBoughtItem
+    const retailer = this.state.mostVisitedRetailer
 
     return(
         <ThemeProvider theme={Theme}>
@@ -53,9 +53,57 @@ render (){
                         <Divider/>
                         <br margin-top ='20px'/>
                         Meist gekaufte Produkte
+                            {items? 
+                                <TableContainer component={Paper}>
+                                    <Table size="small" aria-label="a dense table">
+                                        <TableHead>
+                                        <TableRow>
+                                            <TableCell>Produktname</TableCell>
+                                            <TableCell>Häufigkeit</TableCell>
+                                        </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                        {items.map((item) => (
+                                            <TableRow key={item.getID()}>
+                                                <TableCell>
+                                                    {item.getName()}
+                                                </TableCell>
+                                                <TableCell>
+                                                    {item.getCommonness()}
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer> 
+                            :null}
                         <Divider/>
                         <br margin-top ='20px'/>
                         Meist besuchter Laden
+                        {retailer? 
+                                <TableContainer component={Paper}>
+                                    <Table size="small" aria-label="a dense table">
+                                        <TableHead>
+                                        <TableRow>
+                                            <TableCell>Ladenname</TableCell>
+                                            <TableCell>Häufigkeit</TableCell>
+                                        </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                        {retailer.map((retailer) => (
+                                            <TableRow key={retailer.getID()}>
+                                                <TableCell>
+                                                    {retailer.getName()}
+                                                </TableCell>
+                                                <TableCell>
+                                                    {retailer.getCommonness()}
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer> 
+                            :null}
                         <Divider/>
                     </div>
                 </Grid>
