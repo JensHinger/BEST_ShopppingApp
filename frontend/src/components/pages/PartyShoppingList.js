985142a04edce97b28425f47ff3dd899d12f3476
johny758 <jk211@hdm-stuttgart.de>
2020-07-25 16:13:36 +0200

    Letzte Veränderung der ListEntryCard kenntlich gemacht

--- frontend/src/components/pages/PartyShoppingList.js
+++ frontend/src/components/pages/PartyShoppingList.js
@@ -19,32 +19,62 @@ class PartyShoppingList extends Component{
         this.state = {
             listentries : null,
             list : null,
+            youngestListEntry: null
         }
     }
 
     componentDidMount(){
         this.getListEntriesByList()
         this.getListName()
-        console.log("log:", this.props.match.params)
+        //console.log("log:", this.props.match.params)
 
     }
 
     getListEntriesByList = () => {
-        console.log("versuchen die List id zu loggen:", this.props.match.params)
+        //console.log("versuchen die List id zu loggen:", this.props.match.params)
         const {listid} =  this.props.match.params
-        console.log("list?:", listid)
+        //console.log("list?:", listid)
         ShoppingAPI.getAPI().getListEntriesByListId(listid).then(listentryBOs =>
-            this.setState({  
-              listentries: listentryBOs})
-              )
+            {return(this.findNewestListEntry(listentryBOs))})
               
+    }
+
+    findNewestListEntry = (listentryBOs) => {
+            var youngestBO = listentryBOs.reduce((a, b) => {
+                return new Date(a.creation_date) > new Date(b.creation_date) ? a : b;
+              })
+            //console.log("xoungest bo frisch ausm reduce: ", youngestBO)
+            listentryBOs.splice(listentryBOs.indexOf(youngestBO), 1)
+            this.setState({youngestListEntry: null})
+            this.setState({listentries: listentryBOs})
+            this.setState({youngestListEntry: youngestBO})
+            //console.log("jüngster  als State:", this.state.youngestListEntry)
+
+            //this.setState({})
+
+    }
+
+    updateListEntryHandler = (updatedListEntry) => {
+        //console.log("entry der gepulled werden soll: ", updatedListEntry)
+        var entries = this.state.listentries
+        entries.push(this.state.youngestListEntry)
+        //console.log("alle entries", entries)
+        var remainingEntries = entries.filter((entry) => entry.getID() != updatedListEntry.getID())
+        //console.log("neuer Entry in der PartyShopping: ", remainingEntries)
+        ShoppingAPI.getAPI().getListEntryById(updatedListEntry.getID()).then(
+            function(newEntry) {remainingEntries.push(newEntry[0])
+                //console.log("remaingentries ergänzt: ", remainingEntries)
+                //console.log("state mit dem Entry der das neue Datum beinhaltet:", newEntry)
+                this.findNewestListEntry(remainingEntries)
+                }.bind(this))
     }
 
     getListName = () => {
         ShoppingAPI.getAPI().getListById(this.props.match.params.listid).then((mylist) => this.setState({list: mylist}))
     }
+
     deleteListEntryHandler = (deletedListEntry) => {
-        console.log("diesen ListEntry haben wir gelöscht:", deletedListEntry.getID())
+        //console.log("diesen ListEntry haben wir gelöscht:", deletedListEntry.getID())
         this.setState({
             listentries: this.state.listentries.filter(listEntry => listEntry.getID() !== deletedListEntry.getID())
         })
@@ -57,11 +87,11 @@ class PartyShoppingList extends Component{
     render(){
 
         //const { classes, list } = this.props;
-        const { listentries, list } = this.state;
-        //console.log("aktuelle liste", this.props.match.params.listid)
+        const { listentries, list, youngestListEntry } = this.state;
+        console.log("youngestlistentry laut render ", youngestListEntry)
         
         return(
-        <div style={{width : "50%", margin : "auto"}}>
+            <div style={{width : "50%", margin : "auto"}}>
             <div>
                 <Grid  container direction={'row'}> 
                     { list ?
@@ -84,8 +114,15 @@ class PartyShoppingList extends Component{
             {
                 listentries ?
                 listentries.length === 0 ? 
-                 <Typography variant="h4"> {"Du hast keine Listeneinträge"} </Typography>:
-                listentries.map((listentry) => <ListEntryCard  onListEntryDeleted = {this.deleteListEntryHandler} listid = {this.props.match.params} listentry = {listentry} key = {listentry.getID()}/>)
+                
+                <Typography variant="h4"> {"Du hast keine Listeneinträge"} </Typography>:
+                <div>
+                    {youngestListEntry ? 
+                    <ListEntryCard  onListEntryUpdated={this.updateListEntryHandler} onListEntryDeleted = {this.deleteListEntryHandler} listid = {this.props.match.params} listentry = {youngestListEntry}/>
+                    :null}
+                <hr />
+                {listentries.map((listentry) => <ListEntryCard  onListEntryUpdated={this.updateListEntryHandler} onListEntryDeleted = {this.deleteListEntryHandler} listid = {this.props.match.params} listentry = {listentry} key = {listentry.getID()}/>)}
+                </div>
                 : null
             }            
         </div>
